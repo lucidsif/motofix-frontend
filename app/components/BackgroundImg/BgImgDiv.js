0a4c178941231ce966dc14dbf30c2c2264b65eee
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2016-12-24 00:50:07 -0500

    bg image is not rendering bug

---
+++ app/components/BackgroundImg/BgImgDiv.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BgImgDiv = styled.div`
+background: url(http://res.cloudinary.com/motocloud/image/upload/v1482555076/motowoman_uuhdtx.png) no-repeat center center fixed;
+background-size: cover;
+-moz-background-size: cover;
+-o-background-size: cover;
+-webkit-background-size: cover;
+`;
+
+export default BgImgDiv;
--- app/containers/Landing/index.js
+++ app/containers/Landing/index.js
@@ -3,20 +3,20 @@
  * Landing
  *
  */
-
 import React from 'react';
 import { connect } from 'react-redux';
 import LandingLogo from 'components/BackgroundImg/LandingLogo';
 import logo from 'components/BackgroundImg/logo@2x.png';
+import BgImgDiv from 'components/BackgroundImg/BgImgDiv';
 
 export class Landing extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div className="background-img">
+      <BgImgDiv>
         <LandingLogo src={logo} alt="landing page logo" />
           Landing page here
         <button className="btn btn-primary"> Get a free quote </button>
-      </div>
+      </BgImgDiv>
     );
   }
 }
