4f8ca093a2fccc257501899a340be9bdcdaacabc
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2016-12-24 17:36:50 -0500

    moved global styled to styles folder

--- app/styles/global-styles.js
+++ app/styles/global-styles.js
@@ -6,6 +6,35 @@ html, body {
   margin:0;
   height:100%;
   overflow:hidden;
+  padding-top: 30px;
+}
+
+.img-wrapper {
+  position: relative;
+ }
+
+.img-overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+
+.img-overlay:before {
+  content: ' ';
+  display: block;
+  /* adjust 'height' to position overlay content vertically */
+  height: 50%;
+}
+
+.nav-link {
+  color: #fafafa;
+}
+
+.navbar-brand {
+  color: #fafafa;
 }
 
   body {
