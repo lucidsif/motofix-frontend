b1ed666be08232b52964c1a55b92368ae93ac0da
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2016-12-25 17:21:17 -0500

    working navbar with react routing

--- app/components/Navbar/index.js
+++ app/components/Navbar/index.js
@@ -5,25 +5,30 @@
 */
 
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { browserHistory, Link } from 'react-router';
+import { push } from 'react-router-redux';
 import { Input, Menu } from 'semantic-ui-react';
 
 class AppNavbar extends Component {
-  state = { activeItem: 'home' }
+  constructor(props) {
+    super(props);
+    this.state = { activeItem: 'home' };
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+    this.handleItemClick = (e, { name }) => {
+      browserHistory.push(name);
+      this.setState({ activeItem: name });
+      this.props.dispatch(push(name));
+    };
+  }
 
   render() {
     const { activeItem } = this.state;
     return (
       <Menu secondary>
         <Menu.Item name="home" active={activeItem === 'home'} onClick={this.handleItemClick} />
-        <Menu.Item name="messages" active={activeItem === 'messages'} onClick={this.handleItemClick} />
+        <Menu.Item name="features" active={activeItem === 'features'} onClick={this.handleItemClick} />
         <Menu.Item name="friends" active={activeItem === 'friends'} onClick={this.handleItemClick} />
         <Menu.Menu position="right">
-          <Menu.Item>
-            <Input icon="search" placeholder="Search..." onClick={this.handleItemClick} />
-          </Menu.Item>
           <Menu.Item name="logout" active={activeItem === 'logout'} onClick={this.handleItemClick} />
         </Menu.Menu>
       </Menu>
