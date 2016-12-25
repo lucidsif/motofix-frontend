bff067eb28d964fb46d979cf7e0243fda5f0f4b3
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2016-12-25 16:43:47 -0500

    working navbar with local state

--- app/components/Navbar/index.js
+++ app/components/Navbar/index.js
@@ -11,18 +11,20 @@ import { Input, Menu } from 'semantic-ui-react';
 class AppNavbar extends Component {
   state = { activeItem: 'home' }
 
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
   render() {
     const { activeItem } = this.state;
     return (
       <Menu secondary>
-        <Menu.Item name="home" active={activeItem === 'home'} />
-        <Menu.Item name="messages" active={activeItem === 'messages'} />
-        <Menu.Item name="friends" active={activeItem === 'friends'} />
+        <Menu.Item name="home" active={activeItem === 'home'} onClick={this.handleItemClick} />
+        <Menu.Item name="messages" active={activeItem === 'messages'} onClick={this.handleItemClick} />
+        <Menu.Item name="friends" active={activeItem === 'friends'} onClick={this.handleItemClick} />
         <Menu.Menu position="right">
           <Menu.Item>
-            <Input icon="search" placeholder="Search..." />
+            <Input icon="search" placeholder="Search..." onClick={this.handleItemClick} />
           </Menu.Item>
-          <Menu.Item name="logout" active={activeItem === 'logout'} />
+          <Menu.Item name="logout" active={activeItem === 'logout'} onClick={this.handleItemClick} />
         </Menu.Menu>
       </Menu>
     );
