4d61c274e12afe176c9e681dfe9ffbf67d5f2fdf
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2016-12-27 01:31:38 -0500

    navbar update

--- app/components/Navbar/index.js
+++ app/components/Navbar/index.js
@@ -23,21 +23,20 @@ class AppNavbar extends Component {
     const { activeItem } = this.state;
     return (
       <Menu secondary>
-        <Menu.Item name="home" active={activeItem === 'home'} onClick={this.handleItemClick} />
+        <Menu.Item name="Landing" active={activeItem === 'home'} onClick={this.handleItemClick} />
         <Menu.Item name="features" active={activeItem === 'features'} onClick={this.handleItemClick} />
-        <Menu.Item name="friends" active={activeItem === 'friends'} onClick={this.handleItemClick} />
         <Menu.Menu position="right">
           <Menu.Item name="logout" active={activeItem === 'logout'} onClick={this.handleItemClick} />
         </Menu.Menu>
-        <Dropdown text="Shopping" pointing className="link item">
+        <Dropdown text="Menu" pointing className="link item">
           <Dropdown.Menu>
-            <Dropdown.Header>Categories</Dropdown.Header>
-            <Dropdown.Item>Home Goods</Dropdown.Item>
-            <Dropdown.Item>Bedroom</Dropdown.Item>
+            <Dropdown.Header>Main</Dropdown.Header>
+            <Dropdown.Item>Dashboard</Dropdown.Item>
+            <Dropdown.Item>New Quote</Dropdown.Item>
             <Dropdown.Divider />
-            <Dropdown.Header>Order</Dropdown.Header>
-            <Dropdown.Item>Status</Dropdown.Item>
-            <Dropdown.Item>Cancellations</Dropdown.Item>
+            <Dropdown.Header>Settings</Dropdown.Header>
+            <Dropdown.Item>Payments</Dropdown.Item>
+            <Dropdown.Item>Help</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </Menu>
