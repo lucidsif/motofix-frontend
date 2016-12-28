9089d61d0157c910c7fdac2de6f31fe5f15133e2
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2016-12-28 14:52:32 -0500

    fixed some eslinting issues

--- app/containers/LoginPage/LoginForm.js
+++ app/containers/LoginPage/LoginForm.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 // Notice that we need redux-form/immutable for the boilerplate
 // http://redux-form.com/6.2.0/examples/immutable/
-import { Field, reduxForm } from 'redux-form/immutable'
+import { Field, reduxForm } from 'redux-form/immutable';
 import messages from './messages';
 import { FormattedMessage } from 'react-intl';
 
 const renderField = ({ input, type, meta: { touched, error } }) => (
-    <div>
-      <input {...input} className="form-control" type={type}/>
-      {touched && error && <span>{error}</span>}
-    </div>
-)
+  <div>
+    <input {...input} className="form-control" type={type} />
+    {touched && error && <span>{error}</span>}
+  </div>
+);
 
 const renderGroup = (field) => (
   <div className="form-group">
@@ -30,7 +30,7 @@ const LoginForm = (props) => {
 
       <div className="form-group">
         <label><FormattedMessage {...messages.password} /></label>
-        <Field name="password" type="password" component={renderField} label="Password"/>
+        <Field name="password" type="password" component={renderField} label="Password" />
       </div>
 
       {/* Render error if any. */}
@@ -40,11 +40,11 @@ const LoginForm = (props) => {
         <button type="submit" className="btn btn-primary" disabled={submitting}><FormattedMessage {...messages.login} /></button>
       </div>
     </form>
-  )
-}
+  );
+};
 
 export default reduxForm({
   // Per Step# 2: http://redux-form.com/6.2.0/docs/GettingStarted.md/
   // A unique identifier for this form
-  form: 'loginForm'
-})(LoginForm)
+  form: 'loginForm',
+})(LoginForm);
--- app/containers/QuoteAddVehicle/index.js
+++ app/containers/QuoteAddVehicle/index.js
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from 'react';
 // Notice that we need redux-form/immutable for the boilerplate
 // http://redux-form.com/6.2.0/examples/immutable/
 import { Field, reduxForm } from 'redux-form/immutable';
 
 const renderField = ({ input, type, meta: { touched, error } }) => (
-    <div>
-      <input {...input} className="form-control" type={type}/>
-      {touched && error && <span>{error}</span>}
-    </div>
-)
+  <div>
+    <input {...input} className="form-control" type={type} />
+    {touched && error && <span>{error}</span>}
+  </div>
+);
 
 const renderGroup = (field) => (
   <div className="form-group">
     <label>{label}</label>
     <Field name={field} type={field} component={renderField} />
   </div>
-)
+);
 
 const LoginForm = (props) => {
   const { error, handleSubmit, submitting } = props
@@ -38,11 +38,11 @@ const LoginForm = (props) => {
         <button type="submit" className="btn btn-primary" disabled={submitting}>Submit</button>
       </div>
     </form>
-  )
-}
+  );
+};
 
 export default reduxForm({
   // Per Step# 2: http://redux-form.com/6.2.0/docs/GettingStarted.md/
   // A unique identifier for this form
-  form: 'loginForm'
-})(LoginForm)
+  form: 'loginForm',
+})(LoginForm);
