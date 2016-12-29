a786faa9081b400c833b07c0b1ff5910a4c55de5
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2016-12-28 21:41:19 -0500

    cors origins error when making query to apollo server

--- app/containers/ApolloTest/index.js
+++ app/containers/ApolloTest/index.js
@@ -5,17 +5,21 @@ import gql from 'graphql-tag';
 
 export class ApolloTest extends React.Component { // eslint-disable-line react/prefer-stateless-function
   static propTypes = {
-    loading: PropTypes.bool,
-    posts: PropTypes.array,
+    // loading: PropTypes.bool,
+    motorcycles: PropTypes.array,
   };
 
   render() {
-    if (this.props.loading) {
+    /*
+    if (!this.props.loading) {
       return <span style={{ fontSize: '1rem' }}>Loading...</span>;
     }
-
-    const posts = this.props.posts.map(
-      (post) => <div style={{ fontSize: '1rem' }}>{post.name} - {post.description}</div>
+    */
+    if (!this.props.motorcycles) {
+      return <div>Loading</div>;
+    }
+    const posts = this.props.motorcycles.map(
+      (motorcycle) => <div style={{ fontSize: '1rem' }}>{motorcycle.model}</div>
     );
 
     return <div>{posts}</div>;
@@ -23,18 +27,20 @@ export class ApolloTest extends React.Component { // eslint-disable-line react/p
 }
 
 const TEST_QUERY = gql`
-  query TestQuery {
-    allPosts {
-      name
-      description
-    }
+  query allMotorcycles($filterByMake: String!, $filterByYear: Int){
+    model
   }
 `;
 
 const withData = graphql(TEST_QUERY, {
-  props: ({ data: { loading, allPosts } }) => ({
-    loading,
-    posts: allPosts,
+  options: {
+    variables: {
+      filterByMake: 'bmw',
+      filterByYear: 2009,
+    },
+  },
+  props: ({ data: { allMotorcycles } }) => ({
+    motorcycles: allMotorcycles,
   }),
 });
 
--- app/graphql/index.js
+++ app/graphql/index.js
@@ -2,11 +2,10 @@ import ApolloClient, { createNetworkInterface } from 'apollo-client';
 
 const client = new ApolloClient({
   networkInterface: createNetworkInterface({
-    uri: 'https://api.graph.cool/simple/v1/ciuwqgi3l08pj0171c7ffh238',
+    uri: 'http://localhost:5000/graphql',
     opts: {
       credentials: 'same-origin',
     },
-    transportBatching: true,
   }),
   reduxRootSelector: (state) => state.get('apollo'),
 });
