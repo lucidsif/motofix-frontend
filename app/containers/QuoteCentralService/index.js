4b7780e6366725ab70c3aaadb10bc00e83395ae9
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-01-03 17:31:00 -0500

    added live cart grid

--- app/containers/QuoteCentralService/index.js
+++ app/containers/QuoteCentralService/index.js
@@ -13,25 +13,44 @@ export class QuoteCentralService extends React.Component { // eslint-disable-lin
   render() {
     return (
       <div>
-    <Grid.Row stretched>
-    <Segment padded="very">
-      <p>Live cart</p>
-      </Segment>
-    </Grid.Row>
-    <Grid.Row>
-    <Segment padded="very">
-      Services
-      </Segment>
-    </Grid.Row>
-    <Grid.Row>
-    <Segment padded="very" attached="bottom">
-      Input search box
-      </Segment>
-    </Grid.Row>
-    <Grid.Row>
-    <p>2 buttons</p>
-    </Grid.Row>
-    </div>
+
+        <Grid.Row>
+          <Segment padded="very">
+            <Grid centered divided="vertically">
+              <Grid.Row>
+                <p>Total: $80.00</p>
+              </Grid.Row>
+
+              <Grid.Row columns={2} divided>
+                <Grid.Column>
+                  <p>Saved: $15.00</p>
+                </Grid.Column>
+                <Grid.Column>
+                  <p>Dealer Price: $65.00</p>
+                </Grid.Column>
+              </Grid.Row>
+
+            </Grid>
+          </Segment>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Segment padded="very">
+            Services
+          </Segment>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Segment padded="very" attached="bottom">
+          Input search box
+          </Segment>
+        </Grid.Row>
+
+        <Grid.Row>
+          <p>2 buttons</p>
+        </Grid.Row>
+
+      </div>
     );
   }
 }
