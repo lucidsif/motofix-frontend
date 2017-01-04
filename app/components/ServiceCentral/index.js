286dd247ce67b20f50813d631ccd45df80307711
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-01-04 01:20:19 -0500

    added service segments and icons (not floating right)

--- app/components/ServiceCentral/index.js
+++ app/components/ServiceCentral/index.js
@@ -5,26 +5,47 @@
 */
 
 import React from 'react';
-import { Grid, Segment, Input } from 'semantic-ui-react';
+import { Grid, Segment, Input, Icon } from 'semantic-ui-react';
 
 
 function ServiceCentral() {
   return (
     <Segment padded="very">
-      <Grid.Row>
-        <Input icon="search" placeholder='Search services' />
-      </Grid.Row>
-      <Grid.Row columns={2}>
-        <Grid.Column>
-          Repairs & Maintenance
-        </Grid.Column>
-        <Grid.Column>
-          Diagnostics & Inspections
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-        <p>Services</p>
-      </Grid.Row>
+      <Grid centered>
+        <Grid.Row>
+          <Input icon="search" placeholder="Search services" />
+        </Grid.Row>
+
+        <Grid.Row columns={2}>
+          <Grid.Column>
+            Repairs & Maintenance
+          </Grid.Column>
+          <Grid.Column>
+            Diagnostics & Inspections
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          Common Services
+        </Grid.Row>
+
+
+        <Grid.Row>
+          <Segment attached="top" textAlign="left">
+            <p>Oil Change
+              <Icon name="add to cart" size="large" />
+            </p>
+          </Segment>
+          <Segment attached textAlign="left">
+            Replace brake pads
+          </Segment>
+          <Segment attached="bottom" textAlign="left">
+            Oil Change
+          </Segment>
+        </Grid.Row>
+
+
+      </Grid>
     </Segment>
   );
 }
