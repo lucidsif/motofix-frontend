1760d36ef3cf17aa2750a26a73fdda5a3b815f05
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-01-08 01:38:50 -0500

    broke the services arr into 2 files

--- app/containers/QuoteCentral/reducer.js
+++ app/containers/QuoteCentral/reducer.js
@@ -9,9 +9,9 @@ import {
   ADD_TO_CART,
   REMOVE_FROM_CART,
 } from './constants';
+import services from './reducerServices';
 
 // centralize array of services
-const services = ['Accessory Installation', 'Air Filter Replacement', 'Brake Pad Replacement', 'Brakes Are Squeaking', 'Chain And Sprocket Replacement', 'Check Engine Or FI Light Is On', 'Clean And Lube Chain', 'Fluids Are Leaking', 'Motorcycle Is Not Starting', 'Motorcycle Is Overheating', 'NY State Inspection', 'Prepurchase Inspection', 'Smoke Or Steam Is Coming Out Of Motorcycle', 'Spongy Braking', 'Suspension Tuning', 'Tire Replacement', 'Valve Adjustment', 'Warning Light Is On', 'Winterization','Oil Change'];
 
 const cart = services.reduce((acc, cur, i) => {
   acc[cur] = { selected: false, laborTime: null };
@@ -20,7 +20,7 @@ const cart = services.reduce((acc, cur, i) => {
 
 const estimate = { serviceTotal: 0, partsTotal: 0, total: 0, dealer: 0, priceSavings: 0, percentSavings: 0 };
 const initialState = fromJS({ cart, estimate });
-// Modify reducer so it uses the 2 parameters - makes serviced selected true and adds price to servicetotal
+
 function quoteCentralReducer(state = initialState, action) {
   switch (action.type) {
     case ADD_TO_CART:
---
+++ app/containers/QuoteCentral/reducerServices.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Sif on 1/8/17.
+ */
+
+// simply all the services that is in the state
+const reducerServices = ['Accessory Installation', 'Air Filter Replacement', 'Brake Pad Replacement', 'Brakes Are Squeaking', 'Chain And Sprocket Replacement', 'Check Engine Or FI Light Is On', 'Clean And Lube Chain', 'Fluids Are Leaking', 'Motorcycle Is Not Starting', 'Motorcycle Is Overheating', 'NY State Inspection', 'Prepurchase Inspection', 'Smoke Or Steam Is Coming Out Of Motorcycle', 'Spongy Braking', 'Suspension Tuning', 'Tire Replacement', 'Valve Adjustment', 'Warning Light Is On', 'Winterization','Oil Change'];
+export default reducerServices;
+
+// TODO: 7/10 make this ds immutable?
+// TODO: 2/10 order in abc
