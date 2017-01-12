d65f688bbf51e60020115b3a9a60a197191596c9
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-01-12 02:25:23 -0500

    added todos for PriceBreakDOwn

--- app/components/PriceBreakDown/index.js
+++ app/components/PriceBreakDown/index.js
@@ -7,34 +7,28 @@
 import React from 'react';
 import { Button, Container, List, Image, Item, Grid, Segment, Accordion, Icon, Label } from 'semantic-ui-react';
 
-function PriceBreakDown() {
+// TODO: dynamically render services and parts from props.
+// TODO: calculate prices
+// TODO: 5/10 Fix css styling so item title is in the vertically aligned in the middle
+
+function PriceBreakDown(props) {
   return (
     <Container>
 
         <List>
             <List.Item>
                 <List.Content floated='left'>
-                    <b>Oil Change</b>
+                    <List.Icon name="settings" size='large'/>
+                </List.Content>
+                <List.Content floated='left'>
+                    <i>Oil Change</i>
                 </List.Content>
                 <List.Content floated='right'>
-                    <b>$45.00</b>
+                    <i>$45.00</i>
                 </List.Content>
                 <List>
                     <List.Item>
-                        <List.Content floated='left'>
-                            <Image size='mini' src={'http://thumbs4.ebaystatic.com/m/mZWdRMwthT-Y_bX9Hjo6G2Q/140.jpg'} />
-                        </List.Content>
-                        <List.Content floated='left' verticalAlign='middle'>
-                            <p>Engine Oil</p>
-                        </List.Content>
-                        <List.Content floated='right'>
-                            <p>$10.00</p>
-                        </List.Content>
-                    </List.Item>
-                    <List.Item>
-                        <List.Content floated='left'>
-                            <Image size='mini' src={'http://thumbs4.ebaystatic.com/m/mZWdRMwthT-Y_bX9Hjo6G2Q/140.jpg'} />
-                        </List.Content>
+                        <Image size='mini' floated='left' src={'http://thumbs4.ebaystatic.com/m/mZWdRMwthT-Y_bX9Hjo6G2Q/140.jpg'} />
                         <List.Content floated='left' verticalAlign='middle'>
                             <p>Engine Oil</p>
                         </List.Content>
