2b5f0b9a639869ffafec6e443fa254450c9152c4
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-01-14 01:08:57 -0500

    created controlled modal form

--- app/components/FormModal/index.js
+++ app/components/FormModal/index.js
@@ -5,7 +5,7 @@
 */
 
 import React from 'react';
-import { Button, Modal, Header, Icon, Input, Form } from 'semantic-ui-react';
+import { Button, Modal, Header, Icon, Form } from 'semantic-ui-react';
 
 
 
@@ -13,10 +13,12 @@ import { Button, Modal, Header, Icon, Input, Form } from 'semantic-ui-react';
 class FormModal extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor(props){
     super(props);
-    this.state = { email: null };
+    this.state = { email: null, modalOpen: false };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
   handleChange(e){
@@ -28,9 +30,23 @@ class FormModal extends React.Component { // eslint-disable-line react/prefer-st
     console.log(`mock submit, email to be send is ${this.state.email}`);
   }
 
+  handleOpen(e){
+    this.setState({ modalOpen: true });
+  }
+
+  handleClose(e) {
+    this.setState({ modalOpen: false });
+  }
+
   render() {
     return (
-      <Modal trigger={<Button>Send to Email</Button>} basic size='small'>
+      <Modal
+        trigger={<Button onClick={this.handleOpen}>Send to Email</Button>}
+        open={this.state.modalOpen}
+        onClose={this.handleClose}
+        basic
+        size='small'
+      >
         <Header icon='mail outline' content='What email should we send your quote to?' />
         <Modal.Content>
           <Form onSubmit={this.handleSubmit}>
@@ -41,7 +57,7 @@ class FormModal extends React.Component { // eslint-disable-line react/prefer-st
           <Button color='green' inverted floated="right" onClick={this.handleSubmit}>
             <Icon name='checkmark' /> Send
           </Button>
-          <Button basic color='red' inverted floated="right">
+          <Button basic color='red' inverted floated="right" onClick={this.handleClose}>
             <Icon name='remove' /> Cancel
           </Button>
         </Modal.Actions>
