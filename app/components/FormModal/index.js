e7df4e591bb39bc21601f551b8a7207bd4cca0ac
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-01-14 01:43:30 -0500

    functioning form modal with no async and message rendering

--- app/components/FormModal/index.js
+++ app/components/FormModal/index.js
@@ -5,24 +5,34 @@
 */
 
 import React from 'react';
-import { Button, Modal, Header, Icon, Form } from 'semantic-ui-react';
+import { Button, Modal, Header, Icon, Form, Message } from 'semantic-ui-react';
 
-
-
-
+// TODO: Create a success and error message - Need to share state with redux or have component colocate with parent
 class FormModal extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor(props){
     super(props);
     this.state = { email: null, modalOpen: false };
 
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleOpen = this.handleOpen.bind(this);
     this.handleClose = this.handleClose.bind(this);
   }
 
+  handleChange(e){
+    this.state = { email: e.target.value };
+  }
+
   handleSubmit(e) {
     e.preventDefault();
-    console.log(`mock submit, email to be send is ${this.state.email}`);
+    let validated = this.validateEmail(this.state.email);
+    if(validated){
+      console.log(`validated internally, submitting email: ${this.state.email}`);
+      return this.handleClose();
+    }
+    else{
+      console.log(`invalidated internally: ${this.state.email}`)
+    }
   }
 
   handleOpen(e){
@@ -31,6 +41,11 @@ class FormModal extends React.Component { // eslint-disable-line react/prefer-st
 
   handleClose(e) {
     this.setState({ modalOpen: false });
+  }
+
+  validateEmail(email) {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
   }
 
   render() {
@@ -45,11 +60,17 @@ class FormModal extends React.Component { // eslint-disable-line react/prefer-st
         <Header icon='mail outline' content='What email should we send your quote to?' />
         <Modal.Content>
           <Form onSubmit={this.handleSubmit}>
-            <Form.Input type='email' placeholder="email address" icon="mail outline" iconPosition="left" />
+            <Form.Input
+              onChange={this.handleChange}
+              type='email'
+              placeholder="email address"
+              icon="mail outline"
+              iconPosition="left"
+            />
           </Form>
         </Modal.Content>
         <Modal.Actions>
-          <Button color='green' inverted floated="right" onClick={this.handleSubmit}>
+          <Button type='submit' color='green' inverted floated="right" onClick={this.handleSubmit}>
             <Icon name='checkmark' /> Send
           </Button>
           <Button basic color='red' inverted floated="right" onClick={this.handleClose}>
