e5914dff9a3aa8574514781e9a3ca89341c460e8
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-01-27 01:15:38 -0500

    linted quotecentral

--- app/containers/QuoteCentral/index.js
+++ app/containers/QuoteCentral/index.js
@@ -29,68 +29,72 @@ import selectVehicleDomain from 'containers/QuoteAddVehicle/selectors';
 export class QuoteCentral extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     // conditional render that will either render loading or addservices component
-    const vehicleSearchTerm = `${this.props.vehicle.year} ${this.props.vehicle.manufacturer} ${this.props.vehicle.model_variant}`
+    const vehicleSearchTerm = `${this.props.vehicle.year} ${this.props.vehicle.manufacturer} ${this.props.vehicle.model_variant}`;
     const loadingMessage = `Loading Services for ${vehicleSearchTerm}`;
-    let renderAddServicesUponRepairTimesFetch = null
-    if(this.props.allRepairTimesLoading){
-      renderAddServicesUponRepairTimesFetch = <Segment>
-        <Dimmer active inverted>
-          <Loader inverted content={loadingMessage} />
-        </Dimmer>
-        <Image src='http://semantic-ui.com/images/wireframe/short-paragraph.png' />
-      </Segment>;
-    }  else {
+    let renderAddServicesUponRepairTimesFetch = null;
+    if (this.props.allRepairTimesLoading) {
+      renderAddServicesUponRepairTimesFetch =
+        (
+          <Segment>
+            <Dimmer active inverted>
+              <Loader inverted content={loadingMessage} />
+            </Dimmer>
+            <Image src="http://semantic-ui.com/images/wireframe/short-paragraph.png" />
+          </Segment>
+        );
+    } else {
       renderAddServicesUponRepairTimesFetch = <AddServices props={this.props} />;
     }
-    //
+    const selectedUnavailableServices = Object.keys(this.props.cart).filter((key) => this.props.cart[key].selected && this.props.cart[key].unavailable)
+      .map((serviceName) => {
+        const spacedServiceName = serviceName.replace(/([a-z])([A-Z])/g, '$1 $2');
+        return (
+          <Message.Item key={serviceName}>
+            {spacedServiceName}
+          </Message.Item>
+        );
+      });
 
-    let selectedUnavailableServices = Object.keys(this.props.cart).filter((key) => {
-      return this.props.cart[key].selected && this.props.cart[key].unavailable
-    }).
-      map((serviceName) => {
-      let spacedServiceName = serviceName.replace(/([a-z])([A-Z])/g, '$1 $2');
-      return (
-      <Message.Item key={serviceName}>
-        {spacedServiceName}
-      </Message.Item>
-      )
-    })
-
-    let conditionalServicesMessage = null
-    if(!this.props.allRepairTimes && !this.props.allRepairTimesLoading){
+    let conditionalServicesMessage = null;
+    if (!this.props.allRepairTimes && !this.props.allRepairTimesLoading) {
       conditionalServicesMessage =
-        <Message negative>
-          <Message.Header> Uh oh! No Server Connection</Message.Header>
-          <Message.Content>
-            Please refresh or try again in a few minutes :(
-          </Message.Content>
-        </Message>
-    } else if(selectedUnavailableServices.length > 0){
-      conditionalServicesMessage =
+        (
+          <Message negative>
+            <Message.Header> Uh oh! No Server Connection</Message.Header>
+            <Message.Content>
+              Please refresh or try again in a few minutes :(
+            </Message.Content>
+          </Message>
+        );
+    } else if (selectedUnavailableServices.length > 0) {
+      conditionalServicesMessage = (
         <Message info>
-        <Message.Header>
-          An instant quote for the <span>{vehicleSearchTerm}</span> is unavailable for the currently selected service(s):
-        </Message.Header>
-        <Message.List>
-          {selectedUnavailableServices}
-        </Message.List>
-        <Message.Content>
-          You can still request a fair quote and we'll send you one you when it is available (feature is currently disabled).
-        </Message.Content>
+          <Message.Header>
+            An instant quote for the <span>{vehicleSearchTerm}</span> is unavailable for the currently selected service(s):
+          </Message.Header>
+          <Message.List>
+            {selectedUnavailableServices}
+          </Message.List>
+          <Message.Content>
+           You can still request a fair quote and we will send you one you when it is available (feature is currently disabled).
+          </Message.Content>
           <Button disabled>Request Custom Quote</Button>
-      </Message>
+        </Message>
+      );
     } else {
       conditionalServicesMessage =
-        <Message hidden>
-          You can't see me
-        </Message>
+        (
+          <Message hidden>
+            You cannot see me
+          </Message>
+        );
     }
 
     return (
       <div>
         {conditionalServicesMessage}
         <QuoteCart props={this.props} />
-          {renderAddServicesUponRepairTimesFetch}
+        {renderAddServicesUponRepairTimesFetch}
         <Button onClick={() => browserHistory.push('/quote/vehicle')} >Change Motorcycle</Button>
         <StyledFormModal client={this.props.client} />
       </div>
@@ -103,8 +107,7 @@ const mapStateToProps = createStructuredSelector({
   cart: selectCart(),
   part: selectPart(),
 });
-// what happens if there is no serviceQuery.response or it fails?
-// mutate props of service to true
+
 function mapDispatchToProps(dispatch) {
   return {
     onCartClick: (service) => {
@@ -115,7 +118,7 @@ function mapDispatchToProps(dispatch) {
     },
     // TODO: change name to something more fitting
     getAndSetLaborTime: (service, laborTime, unavailable) => {
-      console.log(`${service} with labortime of ${laborTime} picked up from query result`)
+      console.log(`${service} with labortime of ${laborTime} picked up from query result`);
       dispatch(setLaborTime(service, laborTime, unavailable));
     },
     onPartsQuery: (service, partsData) => {
@@ -126,6 +129,14 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
+QuoteCentral.propTypes = {
+  vehicle: React.PropTypes.object,
+  cart: React.PropTypes.object,
+  allRepairTimes: React.PropTypes.object,
+  allRepairTimesLoading: React.PropTypes.bool,
+  client: React.PropTypes.object,
+};
+
 const RepairTimesQuery = gql`
   query allRepairTimes($midID: String) {
     allRepairTimes(midID: $midID){
@@ -134,15 +145,16 @@ const RepairTimesQuery = gql`
   }
 `;
 
-const QuoteCentralRedux = connect(mapStateToProps, mapDispatchToProps)
+const QuoteCentralRedux = connect(mapStateToProps, mapDispatchToProps);
 
 const withRepairTimesData = graphql(RepairTimesQuery, {
   options: (ownProps) => ({ variables: { midID: ownProps.vehicle.mid } }),
   props: ({ ownProps, data: { loading, allRepairTimes } }) => ({
     allRepairTimesLoading: loading,
     allRepairTimes,
+    ownProps,
   }),
-})
+});
 
 
 export default compose(
