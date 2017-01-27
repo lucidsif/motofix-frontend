01aabd2e7c0b9a6bbf74063e96080a29b840ac1d
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-01-26 23:55:58 -0500

    linted privacy policy

--- app/containers/PrivacyPolicy/index.js
+++ app/containers/PrivacyPolicy/index.js
@@ -5,7 +5,6 @@
  */
 
 import React from 'react';
-import { connect } from 'react-redux';
 
 export class PrivacyPolicy extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
@@ -15,7 +14,7 @@ export class PrivacyPolicy extends React.Component { // eslint-disable-line reac
         motofixes.com Privacy Policy
         </h1>
         <p>
-        This privacy policy has been compiled to better serve those who are concerned with how their 'Personally Identifiable Information' (PII) is being used online. PII, as described in US privacy law and information security, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.
+        This privacy policy has been compiled to better serve those who are concerned with how their Personally Identifiable Information (PII) is being used online. PII, as described in US privacy law and information security, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.
         </p>
         <p>
         What personal information do we collect from the people that visit our blog, website or app?
@@ -69,7 +68,7 @@ export class PrivacyPolicy extends React.Component { // eslint-disable-line reac
         All transactions are processed through a gateway provider and are not stored or processed on our servers.
         </p>
         <p>
-        Do we use 'cookies'?
+        Do we use cookies?
         </p>
 
         <p>
@@ -77,7 +76,7 @@ export class PrivacyPolicy extends React.Component { // eslint-disable-line reac
         </p>
 
         <p>
-        You can choose to have your computer warn you each time a cookie is being sent, or you can choose to turn off all cookies. You do this through your browser settings. Since browser is a little different, look at your browser's Help Menu to learn the correct way to modify your cookies.
+        You can choose to have your computer warn you each time a cookie is being sent, or you can choose to turn off all cookies. You do this through your browser settings. Since browser is a little different, look at your browsers Help Menu to learn the correct way to modify your cookies.
         </p>
         <p>
         If you turn cookies off, some features will be disabled. that make your site experience more efficient and may not function properly.
@@ -85,32 +84,32 @@ export class PrivacyPolicy extends React.Component { // eslint-disable-line reac
         <p>
         However, you will still be able to place orders .
         </p>
-<p>
+        <p>
         Third-party disclosure
-</p>
-<p>
-        We do not sell, trade, or otherwise transfer to outside parties your Personally Identifiable Information unless we provide users with advance notice. This does not include website hosting partners and other parties who assist us in operating our website, conducting our business, or serving our users, so long as those parties agree to keep this information confidential. We may also release information when it's release is appropriate to comply with the law, enforce our site policies, or protect ours or others' rights, property or safety.
-</p>
+        </p>
+        <p>
+        We do not sell, trade, or otherwise transfer to outside parties your Personally Identifiable Information unless we provide users with advance notice. This does not include website hosting partners and other parties who assist us in operating our website, conducting our business, or serving our users, so long as those parties agree to keep this information confidential. We may also release information when its release is appropriate to comply with the law, enforce our site policies, or protect ours or others rights, property or safety.
+        </p>
         <p>
         However, non-personally identifiable visitor information may be provided to other parties for marketing, advertising, or other uses.
         </p>
         <p>
         Third-party links
         </p>
-<p>
+        <p>
         Occasionally, at our discretion, we may include or offer third-party products or services on our website. These third-party sites have separate and independent privacy policies. We therefore have no responsibility or liability for the content and activities of these linked sites. Nonetheless, we seek to protect the integrity of our site and welcome any feedback about these sites.
-</p>
+        </p>
         <p>
         Google
         </p>
-<p>
-        Google's advertising requirements can be summed up by Google's Advertising Principles. They are put in place to provide a positive experience for users. https://support.google.com/adwordspolicy/answer/1316548?hl=en
-</p>
+        <p>
+        Googles advertising requirements can be summed up by Googles Advertising Principles. They are put in place to provide a positive experience for users. https://support.google.com/adwordspolicy/answer/1316548?hl=en
+        </p>
         <p>
         We use Google AdSense Advertising on our website.
         </p>
         <p>
-        Google, as a third-party vendor, uses cookies to serve ads on our site. Google's use of the DART cookie enables it to serve ads to our users based on previous visits to our site and other sites on the Internet. Users may opt-out of the use of the DART cookie by visiting the Google Ad and Content Network privacy policy.
+        Google, as a third-party vendor, uses cookies to serve ads on our site. Googles use of the DART cookie enables it to serve ads to our users based on previous visits to our site and other sites on the Internet. Users may opt-out of the use of the DART cookie by visiting the Google Ad and Content Network privacy policy.
         </p>
         <p>
         We have implemented the following:
@@ -119,9 +118,9 @@ export class PrivacyPolicy extends React.Component { // eslint-disable-line reac
         • Demographics and Interests Reporting
         • DoubleClick Platform Integration
         </p>
-<p>
+        <p>
         We, along with third-party vendors such as Google use first-party cookies (such as the Google Analytics cookies) and third-party cookies (such as the DoubleClick cookie) or other third-party identifiers together to compile data regarding user interactions with ad impressions and other ad service functions as they relate to our website.
-</p>
+        </p>
         <p>
         Opting out:
         Users can set preferences for how Google advertises to you using the Google Ad Settings page. Alternatively, you can opt out by visiting the Network Advertising Initiative Opt Out page or by using the Google Analytics Opt Out Browser add on.
@@ -130,13 +129,13 @@ export class PrivacyPolicy extends React.Component { // eslint-disable-line reac
         California Online Privacy Protection Act
         </p>
         <p>
-        CalOPPA is the first state law in the nation to require commercial websites and online services to post a privacy policy. The law's reach stretches well beyond California to require any person or company in the United States (and conceivably the world) that operates websites collecting Personally Identifiable Information from California consumers to post a conspicuous privacy policy on its website stating exactly the information being collected and those individuals or companies with whom it is being shared. - See more at: http://consumercal.org/california-online-privacy-protection-act-caloppa/#sthash.0FdRbT51.dpuf
+        CalOPPA is the first state law in the nation to require commercial websites and online services to post a privacy policy. The laws reach stretches well beyond California to require any person or company in the United States (and conceivably the world) that operates websites collecting Personally Identifiable Information from California consumers to post a conspicuous privacy policy on its website stating exactly the information being collected and those individuals or companies with whom it is being shared. - See more at: http://consumercal.org/california-online-privacy-protection-act-caloppa/#sthash.0FdRbT51.dpuf
         </p>
         <p>
         According to CalOPPA, we agree to the following:
         Users can visit our site anonymously.
         Once this privacy policy is created, we will add a link to it on our home page or as a minimum, on the first significant page after entering our website.
-        Our Privacy Policy link includes the word 'Privacy' and can easily be found on the page specified above.
+        Our Privacy Policy link includes the word Privacy and can easily be found on the page specified above.
         </p>
         <p>
         You will be notified of any Privacy Policy changes:
@@ -151,13 +150,13 @@ export class PrivacyPolicy extends React.Component { // eslint-disable-line reac
         </p>
         <p>
         Does our site allow third-party behavioral tracking?
-        It's also important to note that we allow third-party behavioral tracking
+        Its also important to note that we allow third-party behavioral tracking
         </p>
         <p>
         COPPA (Children Online Privacy Protection Act)
         </p>
         <p>
-        When it comes to the collection of personal information from children under the age of 13 years old, the Children's Online Privacy Protection Act (COPPA) puts parents in control. The Federal Trade Commission, United States' consumer protection agency, enforces the COPPA Rule, which spells out what operators of websites and online services must do to protect children's privacy and safety online.
+        When it comes to the collection of personal information from children under the age of 13 years old, the Childrens Online Privacy Protection Act (COPPA) puts parents in control. The Federal Trade Commission, United States consumer protection agency, enforces the COPPA Rule, which spells out what operators of websites and online services must do to protect childrens privacy and safety online.
         </p>
         <p>
         We do not specifically market to children under the age of 13 years old.
@@ -182,14 +181,14 @@ export class PrivacyPolicy extends React.Component { // eslint-disable-line reac
         <p>
         The CAN-SPAM Act is a law that sets the rules for commercial email, establishes requirements for commercial messages, gives recipients the right to have emails stopped from being sent to them, and spells out tough penalties for violations.
         </p>
-<p>
+        <p>
         We collect your email address in order to:
         • Send information, respond to inquiries, and/or other requests or questions
         • Process orders and to send information and updates pertaining to orders.
         • Send you additional information related to your product and/or service
         • Market to our mailing list or continue to send emails to our clients after the original transaction has occurred.
-</p>
-<p>
+        </p>
+        <p>
         To be in accordance with CANSPAM, we agree to the following:
         • Not use false or misleading subjects or email addresses.
         • Identify the message as an advertisement in some reasonable way.
@@ -197,7 +196,7 @@ export class PrivacyPolicy extends React.Component { // eslint-disable-line reac
         • Monitor third-party email marketing services for compliance, if one is used.
         • Honor opt-out/unsubscribe requests quickly.
         • Allow users to unsubscribe by using the link at the bottom of each email.
-</p>
+        </p>
 
         <p>
         If at any time you would like to unsubscribe from receiving future emails, you can email us at
@@ -205,16 +204,16 @@ export class PrivacyPolicy extends React.Component { // eslint-disable-line reac
         </p>
 
         Contacting Us
-<p>
+        <p>
         If there are any questions regarding this privacy policy, you may contact us using the information below.
-</p>
-<p>
+        </p>
+        <p>
         motofixes.com
         11518 144th Street
         New York, New York 11436
         United States
         tawsif@motofixes.com
-</p>
+        </p>
 
         Last Edited on 2017-01-26
       </div>
@@ -222,11 +221,4 @@ export class PrivacyPolicy extends React.Component { // eslint-disable-line reac
   }
 }
 
-
-function mapDispatchToProps(dispatch) {
-  return {
-    dispatch,
-  };
-}
-
-export default connect(null, mapDispatchToProps)(PrivacyPolicy);
+export default PrivacyPolicy;
