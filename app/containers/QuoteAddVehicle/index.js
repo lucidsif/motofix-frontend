f60b7ca30d4099b1315d117eaaf80dafe716074b
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-01-29 17:57:31 -0500

    added async errror message in quoteaddvehicle

--- app/containers/QuoteAddVehicle/index.js
+++ app/containers/QuoteAddVehicle/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withApollo, compose } from 'react-apollo';
 import gql from 'graphql-tag';
-import { Button } from 'semantic-ui-react';
+import { Button, Message } from 'semantic-ui-react';
 import { browserHistory } from 'react-router';
 import { addVehicle } from './actions';
 import Select from 'react-select';
@@ -15,6 +15,7 @@ let subModelData = [];
 let subModelFactory = [];
 let motorcycle;
 
+// TODO: 7.1/10 add form validation
 // TODO: 7/10 half the size of the select menus
 // TODO: 6.5/10 refactor to use official api for select menus when available or write in more declarative way
 // TODO: 6/10 replace spans with labels
@@ -31,12 +32,14 @@ class QuoteAddVehicle extends React.Component {
       modelOptions: null,
       subModelOptions: null,
       yearOptions: null,
+      asyncError: false,
     };
 
     this.updateManufacturerValueAndGetModels = this.updateManufacturerValueAndGetModels.bind(this);
     this.updateModelValueAndGetSubModels = this.updateModelValueAndGetSubModels.bind(this);
     this.updateSubModelValueAndRenderYears = this.updateSubModelValueAndRenderYears.bind(this);
     this.updateYear = this.updateYear.bind(this);
+    this.conditionalAsyncErrorMessage = this.conditionalAsyncErrorMessage.bind(this);
   }
   updateManufacturerValueAndGetModels(newValue) {
     this.setState({ manufacturerValue: newValue });
@@ -59,7 +62,11 @@ class QuoteAddVehicle extends React.Component {
       modelData = result.data.allModels;
       modelsFactory = modelData.map((bike) => ({ value: bike.model_id, label: bike.model }));
       this.setState({ modelOptions: modelsFactory });
-    });
+    })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ asyncError: true });
+      });
   }
   // add logic here somehow to generate a years array or maybe anothe func
   updateModelValueAndGetSubModels(newValue) {
@@ -90,7 +97,11 @@ class QuoteAddVehicle extends React.Component {
         return { value: bike.mid, label: bikeLabel };
       });
       this.setState({ subModelOptions: subModelFactory });
-    });
+    })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ asyncError: true });
+      });
   }
 
   updateSubModelValueAndRenderYears(newValue) {
@@ -119,9 +130,23 @@ class QuoteAddVehicle extends React.Component {
     this.setState({ yearValue: newValue });
   }
 
+  conditionalAsyncErrorMessage() {
+    const error = this.state.asyncError;
+    if (error) {
+      return (
+        <Message negative>
+          <p>Warning: Max API calls reached for the day :(</p>
+          <p>We have a limited # of API calls to our data provider until they upgrade us. Please try again tomorrow.</p>
+        </Message>
+      );
+    }
+    return null;
+  }
+
   render() {
     return (
       <form onSubmit={this.props.onSubmitForm}>
+        {this.conditionalAsyncErrorMessage()}
         <h3 className="section-heading">Add your motorcycle</h3>
         <div>
           <span>Select a make</span>
@@ -186,40 +211,39 @@ class QuoteAddVehicle extends React.Component {
 }
 
 /*
-QuoteAddVehicle.propTypes = {
-  loading: React.PropTypes.bool,
-  motorcycles: React.PropTypes.array,
-};
-*/
+ QuoteAddVehicle.propTypes = {
+ loading: React.PropTypes.bool,
+ motorcycles: React.PropTypes.array,
+ };
+ */
 
 export function mapDispatchToProps(dispatch) {
   return {
     onSubmitForm: (evt) => {
       /*
-      evt.preventDefault();
-      console.log('mock vehicle selected and merged to state');
-      const vehicle = {
-        mid: 'HDA06327',
-        manufacturer: 'Honda',
-        model: 'CBR',
-        model_variant: 'CBR600',
-        tuning_description: 'SE',
-        start_year: 2008,
-        end_year: 2011,
-        year: 2010,
-      };
-      dispatch(addVehicle(vehicle));
-      browserHistory.push('/quote/services');
-      */
+       evt.preventDefault();
+       console.log('mock vehicle selected and merged to state');
+       const vehicle = {
+       mid: 'HDA06327',
+       manufacturer: 'Honda',
+       model: 'CBR',
+       model_variant: 'CBR600',
+       tuning_description: 'SE',
+       start_year: 2008,
+       end_year: 2011,
+       year: 2010,
+       };
+       dispatch(addVehicle(vehicle));
+       browserHistory.push('/quote/services');
+       */
       evt.preventDefault();
       if (motorcycle) {
         console.log('all fields submitted');
-        console.log(motorcycle)
+        console.log(motorcycle);
         dispatch(addVehicle(motorcycle));
         browserHistory.push('/quote/services');
       } else {
         console.log('please fill out all fields');
-        // browserHistory.push('/quote/vehicle');
       }
     },
   };
