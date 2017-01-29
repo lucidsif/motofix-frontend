3c849ff2af0e230eab50cb89422ec63e674c757c
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-01-28 22:52:58 -0500

    replaced networkinterface with production server url

--- app/graphql/index.js
+++ app/graphql/index.js
@@ -2,7 +2,7 @@ import ApolloClient, { createNetworkInterface } from 'apollo-client';
 
 const client = new ApolloClient({
   networkInterface: createNetworkInterface({
-    uri: 'http://panda.motofix-backend.development.c66.me/graphql',
+    uri: 'http://162.243.173.35/graphql',
     opts: {
       credentials: 'same-origin',
       shouldBatch: false,
