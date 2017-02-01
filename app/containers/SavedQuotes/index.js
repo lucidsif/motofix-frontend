40e22603bcd014a8bfe289cd1866426ba9ac34e6
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-02-01 03:12:37 -0500

    linted savedquotes. pre graphql wrap

--- app/containers/SavedQuotes/index.js
+++ app/containers/SavedQuotes/index.js
@@ -7,7 +7,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { selectAuthenticated } from 'containers/App/selectors';
-import { createSelector, createStructuredSelector } from 'reselect';
+import { createStructuredSelector } from 'reselect';
 
 import { routerActions } from 'react-router-redux';
 import { UserAuthWrapper } from 'redux-auth-wrapper';
@@ -15,16 +15,15 @@ import { UserAuthWrapper } from 'redux-auth-wrapper';
 // TODO: Require authentication to see this page. redirect to login if inauthenticated
 // TODO: Wrap container with graphql, fetch quotes on mount, and map the quotes.
 
-const UserIsAuthenticated = UserAuthWrapper({
-  authSelector: state => state.get('global').toJS(),
-  predicate: state => state.authenticated,
+const UserIsAuthenticated = UserAuthWrapper({ // eslint-disable-line new-cap
+  authSelector: (state) => state.get('global').toJS(),
+  predicate: (state) => state.authenticated,
   redirectAction: routerActions.replace,
   wrapperDisplayName: 'UserIsAuthenticated',
 });
 
 export class SavedQuotes extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
-    const authenticated = this.props.authenticated;
     console.log(this.props);
     return (
       <div>
@@ -34,10 +33,6 @@ export class SavedQuotes extends React.Component { // eslint-disable-line react/
   }
 }
 
-SavedQuotes.propTypes = {
-  authenticated: React.PropTypes.bool,
-  authData: React.PropTypes.object,
-};
 
 const mapStateToProps = createStructuredSelector({
   authenticated: selectAuthenticated(),
@@ -45,7 +40,7 @@ const mapStateToProps = createStructuredSelector({
 
 const SavedQuotesConnect = connect(mapStateToProps, null)(SavedQuotes);
 
-export default UserIsAuthenticated(SavedQuotesConnect);
+export default UserIsAuthenticated(SavedQuotesConnect); // eslint-disable-line new-cap
 
 /*
 export default UserIsAuthenticated(connect(createSelector(
