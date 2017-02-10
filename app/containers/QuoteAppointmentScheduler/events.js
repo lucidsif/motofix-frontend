ce93dc6ae79e062ceb2005a057a739473380532c
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-02-10 01:38:00 -0500

    integrated drag and drop calendar for quoteappointmentscheduler

---
+++ app/containers/QuoteAppointmentScheduler/events.js
@@ -0,0 +1,82 @@
+export default [
+  {
+    title: 'N/A',
+    allDay: true,
+    start: new Date(2015, 3, 0),
+    end: new Date(2015, 3, 1),
+    category: 'unavailable',
+  },
+  {
+    title: 'N/A',
+    start: new Date(2015, 3, 7),
+    end: new Date(2015, 3, 10),
+    category: 'unavailable',
+  },
+
+  {
+    title: 'N/A',
+    start: new Date(2017, 1, 10, 0, 0, 0),
+    end: new Date(2017, 1, 12, 0, 0, 0),
+    category: 'unavailable',
+  },
+
+  {
+    title: 'N/A',
+    start: new Date(2016, 10, 6, 0, 0, 0),
+    end: new Date(2016, 10, 13, 0, 0, 0),
+    category: 'unavailable',
+  },
+
+  {
+    title: 'N/A',
+    start: new Date(2015, 3, 9, 0, 0, 0),
+    end: new Date(2015, 3, 9, 0, 0, 0),
+    category: 'unavailable',
+  },
+  {
+    title: 'N/A',
+    start: new Date(2015, 3, 11),
+    end: new Date(2015, 3, 13),
+    desc: 'Big conference for important people',
+    category: 'unavailable',
+  },
+  {
+    title: 'N/A',
+    start: new Date(2017, 1, 10, 10, 30, 0, 0),
+    end: new Date(2017, 1, 10, 12, 30, 0, 0),
+    desc: 'Pre-meeting meeting, to prepare for the meeting',
+    category: 'unavailable',
+  },
+  {
+    title: 'Me',
+    start: new Date(2015, 3, 12, 12, 0, 0, 0),
+    end: new Date(2015, 3, 12, 13, 0, 0, 0),
+    desc: 'Power lunch',
+    category: 'appointment',
+  },
+  {
+    title: 'N/A',
+    start: new Date(2015, 3, 12, 14, 0, 0, 0),
+    end: new Date(2015, 3, 12, 15, 0, 0, 0),
+    category: 'unavailable',
+  },
+  {
+    title: 'N/A',
+    start: new Date(2015, 3, 12, 17, 0, 0, 0),
+    end: new Date(2015, 3, 12, 17, 30, 0, 0),
+    desc: 'Most important meal of the day',
+    category: 'unavailable',
+  },
+  {
+    title: 'N/A',
+    start: new Date(2015, 3, 12, 20, 0, 0, 0),
+    end: new Date(2015, 3, 12, 21, 0, 0, 0),
+    category: 'unavailable',
+  },
+  {
+    title: 'N/A',
+    start: new Date(2015, 3, 13, 7, 0, 0),
+    end: new Date(2015, 3, 13, 10, 30, 0),
+    category: 'unavailable',
+  },
+];
--- app/containers/QuoteAppointmentScheduler/index.js
+++ app/containers/QuoteAppointmentScheduler/index.js
@@ -8,11 +8,92 @@ import React from 'react';
 import { connect } from 'react-redux';
 import selectQuoteAppointmentScheduler from './selectors';
 
+import BigCalendar from 'react-big-calendar';
+import events from './events';
+import HTML5Backend from 'react-dnd-html5-backend';
+import { DragDropContext } from 'react-dnd';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
+
+import moment from 'moment';
+
+BigCalendar.momentLocalizer(moment);
+// a. disable filled time slots/change stlying
+//
+// TODO: disable selecting alldays across
+
+// import 'react-big-calendar/lib/addons/dragAndDrop/styles.less';
+
+const DragAndDropCalendar = withDragAndDrop(BigCalendar);
+
 export class QuoteAppointmentScheduler extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props)
+    this.state = {
+      events,
+    }
+
+    this.moveEvent = this.moveEvent.bind(this);
+    this.changeStyling = this.changeStyling.bind(this);
+  }
+
+  moveEvent({ event, start, end }) {
+    console.log(event);
+    const { events } = this.state;
+    if(event.category === 'appointment') {
+      const idx = events.indexOf(event);
+      const updatedEvent = { ...event, start, end };
+
+      const nextEvents = [...events]
+      nextEvents.splice(idx, 1, updatedEvent);
+
+      this.setState({
+        events: nextEvents,
+      });
+      console.log(`${event.title} was dropped onto ${event.start}`);
+    }
+    else {
+      console.log(`${event.title} cannot be selected!`);
+    }
+  }
+
+  changeStyling(e) {
+    console.log('in changeStlying');
+    console.log(e);
+  }
+
   render() {
+    const todaysDate = moment().format('MMMM Do YYYY');
+    let formats = {
+      dateFormat: 'MMM Do',
+
+      dayFormat: (date, culture, localizer) =>
+        localizer.format(date, 'MMM Do', culture),
+    }
     return (
       <div>
-        calendar here
+        <h3 className="callout">
+          Schedule Appointment
+        </h3>
+        <DragAndDropCalendar
+          formats={formats}
+          selectable
+          events={this.state.events}
+          onSelectEvent={event => {
+            if(event.category !== 'appointment'){
+              return false;
+            }
+          }}
+          onEventDrop={this.moveEvent}
+          eventPropGetter={
+            event => ({className: 'category-' + event.category.toLowerCase()})
+          }
+          views={['week', 'day']}
+          defaultView='week'
+          defaultDate={new Date(2015, 3, 12)}
+          min={new Date(1970, 1, 1, 7) }
+          max={new Date(1970, 1, 1, 19) }
+          onHover={(e) => console.log(e)}
+        />
       </div>
     );
   }
